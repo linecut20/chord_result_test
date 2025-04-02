55f85af9e94efbdbe4b06ae45c4e1b1f0627e3fc
linecut20 <80865818+linecut20@users.noreply.github.com>
2025-04-02 10:31:36 +0900

    new data (#1)
    * flat parsing
    
    * a
    
    * new data

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:chord_result_test/detail_page.dart';
+import 'package:chord_result_test/new_data.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            final Map parsed = newParseData[index];
+
+            return ListTile(
+              title: Text(parsed['title']),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailPage(data: parsed),
+                  ),
+                );
+              },
+            );
+          },
+          itemCount: newParseData.length,
+        ),
+      ),
+    );
+  }
+}
